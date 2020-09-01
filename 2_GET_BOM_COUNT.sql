1d84343a802eb55a0be16ca3a4babfb8625ad5ed
mohammad_darwish <mohammad_darwish@siliconexpert.com>
2020-09-02 00:02:39 +0200

    Fix issues for performance

--- 2_GET_BOM_COUNT.sql
+++ 2_GET_BOM_COUNT.sql
@@ -1,35 +1,74 @@
+
+USE opendatasheets;
 DROP FUNCTION IF EXISTS GET_BOM_COUNT;
 
-use opendatasheets;
- 
-DELIMITER $$
-CREATE FUNCTION GET_BOM_COUNT(com_id_in integer(100), site_id_in integer(100), uploaded_mpn_in  VARCHAR(255), uploaded_supplier_in VARCHAR(255), man_id_in integer(11))
-   RETURNS integer(100)
-   DETERMINISTIC
-   BEGIN
-      DECLARE count_bom_id   integer(100);
 
-	IF com_id_in < 1 AND (uploaded_mpn_in is NULL OR uploaded_mpn_in = '')  AND (uploaded_supplier_in is  NULL OR uploaded_supplier_in = '')
-	AND uploaded_mpn_in < 1
-	THEN
-	RETURN 0;
-	END IF;
-      
-	  SELECT 
-	  	count(distinct br.bom_id, br.com_id)
+DELIMITER $$
+
+CREATE FUNCTION GET_BOM_COUNT(com_id_in              INTEGER(100),
+                              site_id_in             INTEGER(100),
+                              uploaded_mpn_in        VARCHAR(255),
+                              uploaded_supplier_in   VARCHAR(255),
+                              man_id_in              INTEGER(11))
+   RETURNS INTEGER(100)
+   DETERMINISTIC
+BEGIN
+   DECLARE count_bom_id   INTEGER(100) DEFAULT 0;
+
+   IF com_id_in > 0
+   THEN
+      SELECT count(DISTINCT br.bom_id, br.com_id)
         INTO count_bom_id
-        FROM bom_result br , bom b WHERE br.bom_id=b.id and
-        Case 
-        	WHEN com_id_in > 0 THEN br.com_id = com_id_in 
-         	WHEN uploaded_mpn_in is not NULL AND uploaded_mpn_in != '' AND   uploaded_supplier_in is not NULL AND uploaded_supplier_in != '' THEN br.uploaded_mpn = uploaded_mpn_in AND br.uploaded_supplier = uploaded_supplier_in
-         	WHEN uploaded_mpn_in is not NULL AND uploaded_mpn_in != '' AND man_id_in > 0 THEN br.uploaded_mpn = uploaded_mpn_in AND br.man_id = man_id_in
-         	WHEN uploaded_mpn_in is not NULL AND uploaded_mpn_in != ''  THEN br.uploaded_mpn = uploaded_mpn_in 
-         END
-         and br.site_id = site_id_in
-           and b.status_flag =0  AND br.status_flag =0;
-	   
-	
-      RETURN count_bom_id;
-   END
+        FROM bom_result br, bom b
+       WHERE     br.bom_id = b.id
+             AND br.com_id = com_id_in
+             AND br.site_id = site_id_in
+             AND b.status_flag = 0
+             AND br.status_flag = 0;
+             
+   ELSEIF     uploaded_mpn_in IS NOT NULL
+          AND uploaded_mpn_in != ''
+          AND uploaded_supplier_in IS NOT NULL
+          AND uploaded_supplier_in != ''
+   THEN
+      SELECT count(DISTINCT br.bom_id, br.com_id)
+        INTO count_bom_id
+        FROM bom_result br, bom b
+       WHERE     br.bom_id = b.id
+             AND br.uploaded_mpn = uploaded_mpn_in
+             AND br.uploaded_supplier = uploaded_supplier_in
+             AND br.site_id = site_id_in
+             AND b.status_flag = 0
+             AND br.status_flag = 0;
+   ELSEIF     uploaded_mpn_in IS NOT NULL
+          AND uploaded_mpn_in != ''
+          AND man_id_in > 0
+   THEN
+      SELECT count(DISTINCT br.bom_id, br.com_id)
+        INTO count_bom_id
+        FROM bom_result br, bom b
+       WHERE     br.bom_id = b.id
+             AND br.uploaded_mpn = uploaded_mpn_in
+             AND br.man_id = man_id_in
+             AND br.site_id = site_id_in
+             AND b.status_flag = 0
+             AND br.status_flag = 0;
+   ELSEIF uploaded_mpn_in IS NOT NULL AND uploaded_mpn_in != ''
+   THEN
+      SELECT count(DISTINCT br.bom_id, br.com_id)
+        INTO count_bom_id
+        FROM bom_result br, bom b
+       WHERE     br.bom_id = b.id
+             AND br.uploaded_mpn = uploaded_mpn_in
+             AND br.site_id = site_id_in
+             AND b.status_flag = 0
+             AND br.status_flag = 0;
+   END IF;
+
+
+
+   RETURN count_bom_id;
+END
 $$
+
 DELIMITER ;
--- 4_CREATE_BOM_PATH_LIST.sql
+++ 4_CREATE_BOM_PATH_LIST.sql
@@ -1,85 +1,161 @@
-use  opendatasheets;
-Drop PROCEDURE IF EXISTS CREATE_BOM_PATH_LIST;
+USE  opendatasheets;
+DROP PROCEDURE IF EXISTS CREATE_BOM_PATH_LIST;
 DELIMITER $$
 
-CREATE PROCEDURE CREATE_BOM_PATH_LIST(IN  com_id_in       INTEGER(100),
-                                      IN  site_id_in integer(100),
-                                       uploaded_mpn_in  VARCHAR(255), uploaded_supplier_in VARCHAR(255), man_id_in integer(11),
-                                      OUT bom_path_list   LONGTEXT)
-   BEGIN
-    DECLARE finished INTEGER DEFAULT 0;
-    DECLARE bom_id_temp INTEGER DEFAULT 0;
-    DECLARE bom_path LONGTEXT DEFAULT "";
-    DECLARE current_bom_name LONGTEXT DEFAULT "";
-    DECLARE result LONGTEXT DEFAULT "";
-    DECLARE item_path_temp LONGTEXT DEFAULT "";
-    DECLARE name_temp LONGTEXT DEFAULT "";
-    DECLARE com_id_temp INTEGER DEFAULT 0;
+CREATE PROCEDURE CREATE_BOM_PATH_LIST(IN      com_id_in       INTEGER(100),
+                                      IN      site_id_in      INTEGER(100),
+                                      uploaded_mpn_in         VARCHAR(255),
+                                      uploaded_supplier_in    VARCHAR(255),
+                                      man_id_in               INTEGER(11),
+                                          OUT bom_path_list   LONGTEXT)
+BEGIN
+   DECLARE finished           INTEGER DEFAULT 0;
+   DECLARE bom_id_temp        INTEGER DEFAULT 0;
+   DECLARE bom_path           LONGTEXT DEFAULT "";
+   DECLARE current_bom_name   LONGTEXT DEFAULT "";
+   DECLARE result             LONGTEXT DEFAULT "";
+   DECLARE item_path_temp     LONGTEXT DEFAULT "";
+   DECLARE name_temp          LONGTEXT DEFAULT "";
+   DECLARE com_id_temp        INTEGER DEFAULT 0;
 
-    DECLARE cur_bom_id
-        CURSOR FOR
-           SELECT distinct br.bom_id, br.com_id FROM bom_result br, bom b WHERE br.bom_id=b.id
-            and
-           Case 
-        	WHEN com_id_in > 0 THEN br.com_id = com_id_in 
-         	WHEN uploaded_mpn_in is not NULL AND uploaded_mpn_in != '' AND   uploaded_supplier_in is not NULL AND uploaded_supplier_in != '' THEN br.uploaded_mpn = uploaded_mpn_in AND br.uploaded_supplier = uploaded_supplier_in
-         	WHEN uploaded_mpn_in is not NULL AND uploaded_mpn_in != '' AND man_id_in > 0 THEN br.uploaded_mpn = uploaded_mpn_in AND br.man_id = man_id_in
-         	WHEN uploaded_mpn_in is not NULL AND uploaded_mpn_in != ''  THEN br.uploaded_mpn = uploaded_mpn_in 
-         END
-             
-             and br.site_id = site_id_in
-           and b.status_flag =0 AND  br.status_flag =0;
 
-    -- declare NOT FOUND handler
-    DECLARE CONTINUE HANDLER
-        FOR NOT FOUND SET finished = 1;
 
-      OPEN cur_bom_id;
+   DECLARE cur_bom_id CURSOR FOR SELECT bom_id, com_id FROM tmp_view_bom;
 
-     getBomPath:
-      LOOP
-      
-         FETCH cur_bom_id   INTO bom_id_temp, com_id_temp;
-             IF finished = 1
+   -- declare NOT FOUND handler
+   DECLARE CONTINUE HANDLER FOR NOT FOUND
+   SET finished = 1;
+
+   IF com_id_in > 0
+   THEN
+      SET @v =
+             concat(
+                'CREATE OR REPLACE VIEW tmp_view_bom as SELECT distinct br.bom_id, br.com_id FROM bom_result br, bom b
+       WHERE     br.bom_id = b.id
+             AND br.com_id =',
+                com_id_in,
+                ' AND br.site_id =',
+                site_id_in,
+                '
+             AND b.status_flag = 0
+             AND br.status_flag = 0');
+
+      PREPARE stm FROM @v;
+
+      EXECUTE stm;
+      DEALLOCATE PREPARE stm;
+   ELSEIF     uploaded_mpn_in IS NOT NULL
+          AND uploaded_mpn_in != ''
+          AND uploaded_supplier_in IS NOT NULL
+          AND uploaded_supplier_in != ''
+   THEN
+      SET @v =
+             concat(
+                'CREATE OR REPLACE VIEW tmp_view_bom as SELECT distinct br.bom_id, br.com_id FROM bom_result br, bom b
+       WHERE     br.bom_id = b.id AND br.uploaded_mpn =',
+                uploaded_mpn_in,
+                ' AND br.uploaded_supplier =',
+                uploaded_supplier_in,
+                ' AND br.site_id = ',
+                site_id_in,
+                '  AND b.status_flag = 0
+             AND br.status_flag = 0');
+
+      PREPARE stm FROM @v;
+
+      EXECUTE stm;
+      DEALLOCATE PREPARE stm;
+   ELSEIF     uploaded_mpn_in IS NOT NULL
+          AND uploaded_mpn_in != ''
+          AND man_id_in > 0
+   THEN
+      SET @v =
+             concat(
+                'CREATE OR REPLACE VIEW tmp_view_bom as SELECT distinct br.bom_id, br.com_id FROM bom_result br, bom b
+       WHERE     br.bom_id = b.id
+             AND br.uploaded_mpn =',
+                uploaded_mpn_in,
+                ' AND br.man_id =',
+                man_id_in,
+                '  AND br.site_id = site_id_in
+             AND b.status_flag = 0
+             AND br.status_flag = 0');
+
+      PREPARE stm FROM @v;
+
+      EXECUTE stm;
+      DEALLOCATE PREPARE stm;
+   ELSEIF uploaded_mpn_in IS NOT NULL AND uploaded_mpn_in != ''
+   THEN
+      SET @v =
+             concat(
+                'CREATE OR REPLACE VIEW tmp_view_bom as SELECT distinct br.bom_id, br.com_id FROM bom_result br, bom b
+       WHERE     br.bom_id = b.id
+             AND br.uploaded_mpn = ',
+                uploaded_mpn_in,
+                ' AND br.site_id =',
+                site_id_in,
+                'AND b.status_flag = 0
+             AND br.status_flag = 0');
+
+      PREPARE stm FROM @v;
+
+      EXECUTE stm;
+      DEALLOCATE PREPARE stm;
+   END IF;
+
+
+
+   OPEN cur_bom_id;
+
+  getBomPath:
+   LOOP
+      FETCH cur_bom_id INTO bom_id_temp, com_id_temp;
+
+      IF finished = 1
+      THEN
+         LEAVE getBomPath;
+      END IF;
+
+      SELECT item_path
+        INTO item_path_temp
+        FROM bom
+       WHERE id = bom_id_temp;
+
+      SELECT name
+        INTO name_temp
+        FROM bom
+       WHERE id = bom_id_temp;
+
+
+      IF item_path_temp IS NULL OR LENGTH(item_path_temp) = 0
+      THEN
+         SET @concatenated_name = name_temp;
+      ELSE
+         CALL GET_ITEM_PATH(item_path_temp, @_item_path);
+
+         IF LENGTH(@_item_path) > 0
          THEN
-            LEAVE getBomPath;
+            SET @concatenated_name = CONCAT(@_item_path, " > ", name_temp);
+         ELSE
+            SET @concatenated_name = name_temp;
          END IF;
-         
-         select item_path into item_path_temp  from bom where id = bom_id_temp;
-         
-         select name into name_temp  from bom where id = bom_id_temp;
-         
+      END IF;
 
-         IF item_path_temp is NULL OR LENGTH(item_path_temp) = 0
-         THEN
-          set  @concatenated_name = name_temp;
-          ELSE
-        
-          call GET_ITEM_PATH(item_path_temp,@_item_path);
-          IF LENGTH(@_item_path) >0 THEN
-          set   @concatenated_name = CONCAT( @_item_path, " > ", name_temp);
-          ELSE
-           set  @concatenated_name = name_temp;
-           END IF;
-         END IF;
-         
-     
-     
-        IF LENGTH(result) > 0 THEN
+
+
+      IF LENGTH(result) > 0
+      THEN
          SET result = CONCAT(result, " , ", @concatenated_name);
-         ELSE 
-         SET result =  @concatenated_name;
-         END IF;
-     
-           
-        
-      END LOOP getBomPath;
+      ELSE
+         SET result = @concatenated_name;
+      END IF;
+   END LOOP getBomPath;
 
-      CLOSE cur_bom_id;
-      set bom_path_list = result;
-    
-      
-   END
+   CLOSE cur_bom_id;
+
+   SET bom_path_list = result;
+END
 $$
 
 DELIMITER ;
